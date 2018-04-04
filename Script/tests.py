e61a6c6ac35b072b6f8b24d6fe8b9551131b712a
Your Name <you@example.com>
2018-04-04 16:44:06 +0200

    going home

--- Script/tests.py
+++ Script/tests.py
@@ -1,11 +1,36 @@
 import urllib
 import urllib2
 import webbrowser
+import contextlib
+import json
 
-url = "http://duckduckgo.com/html"
-data = urllib.urlencode({'q': 'Python'})
-results = urllib2.urlopen(url, data)
+login_url = "http://127.0.0.1:8000/workflowFile_add/"
+fileName = '/home/roberto/sda/ScipionUserData/projects/AD41_PDB/workflow.json'
+cookies = urllib2.HTTPCookieProcessor()
+opener = urllib2.build_opener(cookies)
+urllib2.install_opener(opener)
+
+opener.open(login_url)
+
+try:
+    token = [x.value for x in cookies.cookiejar if x.name == 'csrftoken'][0]
+except IndexError:
+    print (False, "no csrftoken")
+
+with open(fileName, 'r') as f:
+    content = f.read()
+
+params =  dict(csrfmiddlewaretoken=token,
+               json=content,
+               jsonFileName='qwerty')
+encoded_params = urllib.urlencode(params)
+
+with contextlib.closing(opener.open(login_url, encoded_params)) as f:
+    html = f.read()
+
 with open("results.html", "w") as f:
-    f.write(results.read())
+    f.write(html)
 
-webbrowser.open("results.html")
+#webbrowser.open("file://results.html")
+
+
