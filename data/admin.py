2e0b39418c8d8994c205757c1aa9695d35b3d378
Your Name <you@example.com>
2018-04-05 11:54:06 +0200

    add models to admn interface

--- data/admin.py
+++ data/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import WorkFlow, Category, Protocol
 # Register your models here.
+
+admin.site.register(WorkFlow)
+admin.site.register(Category)
+admin.site.register(Protocol)
