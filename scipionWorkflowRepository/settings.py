cd2329e9f59715d991274dca14e75cc6fed31129
Your Name <you@example.com>
2018-04-09 18:35:21 +0200

    use heroku

--- scipionWorkflowRepository/settings.py
+++ scipionWorkflowRepository/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = '19j8)(*pq=60@u-fg==*wl0#*2g^7882!&8xy@&pe@*%1cfy)w'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', 'workflows.scipion.i2pc.es', 'warm-sea-81539.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1','workflows.scipion.i2pc.es']
 
 
 # Application definition
@@ -79,16 +79,19 @@ WSGI_APPLICATION = 'scipionWorkflowRepository.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        # save database to disk
-        'TEST': {
-            'NAME': 'test_db.sqlite',
-        },
-    }
-}
+#DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#        # save database to disk
+#        'TEST': {
+#            'ENGINE': 'django.db.backends.sqlite3',
+#            'NAME': 'test_db.sqlite',
+#        },
+#    }
+#}
+DATABASES = {}
+DATABASES['default'] = dj_database_url.config(default='postgres://alumnodb:alumnodb@localhost:5432/onlineshop')
 
 
 # Password validation
