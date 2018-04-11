d6fde25dba52fe4c44aa3ecfd1ed6198b0997268
Your Name <you@example.com>
2018-04-11 12:22:40 +0200

    se several sessions

--- scipionWorkflowRepository/settings_heroku.py
+++ scipionWorkflowRepository/settings_heroku.py
@@ -32,7 +32,7 @@ SECRET_KEY = '19j8)(*pq=60@u-fg==*wl0#*2g^7882!&8xy@&pe@*%1cfy)w'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1','workflows.scipion.i2pc.es']
+ALLOWED_HOSTS = ['127.0.0.1','workflows.scipion.i2pc.es','warm-sea-81539.herokuapp.com']
 
 
 # Application definition
